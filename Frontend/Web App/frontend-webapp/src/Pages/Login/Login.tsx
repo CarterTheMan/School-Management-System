59c736e0b51bc0780b16ca8010f113d0541983cd
CarterTheMan <cartermoseley20@gmail.com>
2024-07-06 15:29:56 -0500

    Added the first bit of routing

--- Frontend/Web App/frontend-webapp/src/Pages/Login/Login.tsx
+++ Frontend/Web App/frontend-webapp/src/Pages/Login/Login.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./Login.css";
+import { useParams } from 'react-router-dom';
 
 export default function Login() {
+  let params = useParams();
+
   return (
     <div>
-      <h1>This is the login page</h1>
+      <h1>This is the login page for user: {params.id}</h1>
     </div>
   );
 }
